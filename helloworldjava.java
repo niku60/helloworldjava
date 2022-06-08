ca7b73c54753b4c06c9d06b6dca13c4bf194e63b
niku60 <66554435+niku60@users.noreply.github.com>
2022-06-08 21:10:12 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps to test webhook - new poll changes");
+    System.out.println("Hello World from DevOps to test webhook - wehook test changes");
     }
 }
 
